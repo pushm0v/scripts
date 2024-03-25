54afe6779866858f2cb7a4e534d7e17e108135f0
Bherly Novrandy <pushm0v.development@gmail.com>
2024-03-25 23:22:06 +0700

    Silent yes

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ### Docker and docker compose prerequisites
-sudo apt-get install curl
-sudo apt-get install gnupg
-sudo apt-get install ca-certificates
-sudo apt-get install lsb-release
+sudo apt-get install -y curl
+sudo apt-get install -y gnupg
+sudo apt-get install -y ca-certificates
+sudo apt-get install -y lsb-release
 
 ### Download the docker gpg file to Ubuntu
 sudo mkdir -p /etc/apt/keyrings
@@ -13,4 +13,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
